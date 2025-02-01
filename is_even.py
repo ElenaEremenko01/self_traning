59ddaaec81ab4b47a7613f2647662f0948bd5f59
Елена <orderkgpremium@gmail.com>
2025-02-01 16:20:47 +0200

    Исправлен ошибки по PEP8, использован flake8, black, isort

--- is_even.py
+++ is_even.py
@@ -1,5 +1,6 @@
 # написать функцию is_even
 
+
 def is_even(number):
     try:
         if number % 2 == 0:
@@ -7,8 +8,8 @@ def is_even(number):
         else:
             return False
     except TypeError:
-        print('Вы можете ввести только число')
+        print("Вы можете ввести только число")
 
 
-if __name__ == '__main__':
-    print(is_even('vif'))
+if __name__ == "__main__":
+    print(is_even("vif"))
--- reverse_words.py
+++ reverse_words.py
@@ -1,12 +1,13 @@
 # функция переворачивает слова, но не меняет их порядок
 
+
 def reverse_words(sentence):
     words = sentence.split()
     reversed_list = []
     for word in words:
         reversed_list.append(word[::-1])
-    print(' '.join(reversed_list))
+    print(" ".join(reversed_list))
 
 
-if __name__ == '__main__':
-    reverse_words('hello world')
+if __name__ == "__main__":
+    reverse_words("hello world")
